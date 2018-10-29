b4c32c422aa9df81d6b825b0f6f4b9b5ade9e775
ariaamz <ariamoradpour.am@gmail.com>
2018-10-29 09:50:13 -0400

    Update src-2.c

--- src-2.c
+++ src-2.c
@@ -1,5 +1,18 @@
 /*
-size
+ * following program should prompt the user to enter some grades ( we should be able to change how many numbers the user enters by changing a single variable )
+ * after the user is finished inputing data it will calculate the average, highest and lowest grade and it will print it to the user
+ * create and set up a visual studio project 
+ * fix any syntax or logical error
+ * after you are done, this program should generate an output similar to the following,
+ * Please enter your grades blow
+ * Grade 1 = 23.65
+ * Grade 2 = 34.65
+ * Grade 3 = 45.98
+ * Grade 4 = 99.99
+ * Grade 5 = 45.87
+ * Highest grade  = 99.990
+ * lowest grade  = 23.650
+ * Average = 50.028
 */
 #include <stdio.h>
 
